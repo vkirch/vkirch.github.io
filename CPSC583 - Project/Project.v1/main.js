c4f854c8c8011fdee97012f737f4e451ec609a71
DESKTOP-6PSTR3V\vanes <vanessa.kirch88@gmail.com>
2019-03-17 14:37:29 -0600

    testing -1* newLength

--- CPSC583 - Project/Project.v1/main.js
+++ CPSC583 - Project/Project.v1/main.js
@@ -165,10 +165,11 @@ function changeBarLengthLife(attr, maxAttrValue){
 function changeBarLengthFertility(attr, maxAttrValue){
     for (var i = 0; i < _data_fertility.length; i++){
         var newLength = mapValue(_data_fertility[i][attr], 0, maxAttrValue, 0, _vis_width/3 - PADDING_FOR_LABELS);
+        newLength = -1*newLength;
+        console.log(newLength);
         var bar = document.getElementById("row_F_" + i);
 
         var oldX = bar.getAttribute("x"); //- newLength; //FOR NEW FUNCTION
-        console.log(oldX);
         var oldLength = bar.getAttribute("width");
         var newX = oldX;
 
@@ -200,58 +201,3 @@ function changeBarLengthFertility(attr, maxAttrValue){
 function mapValue(value, origMin, origMax, newMin, newMax){
     return (value - origMin) * (newMax - newMin) / (origMax - origMin) + newMin;
 }
-
-/*
-
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-// set the ranges
-var y = d3.scaleBand()
-    .range([height, 0])
-    .padding(0.1);
-
-var x = d3.scaleLinear()
-    .range([0, width]);
-
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-// format the data
-data.forEach(function(d) {
-    d.sales = +d.sales;
-});
-
-// Scale the range of the data in the domains
-x.domain([0, d3.max(data, function(d){ return d.sales; })])
-y.domain(data.map(function(d) { return d.salesperson; }));
-//y.domain([0, d3.max(data, function(d) { return d.sales; })]);
-
-// append the rectangles for the bar chart
-svg.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    //.attr("x", function(d) { return x(d.sales); })
-    .attr("width", function(d) {return x(d.sales); } )
-    .attr("y", function(d) { return y(d.salesperson); })
-    .attr("height", y.bandwidth());
-
-// add the x Axis
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-// add the y Axis
-svg.append("g")
-    .call(d3.axisLeft(y));
-*/
