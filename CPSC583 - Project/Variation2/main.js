24925f5facc507f75e7e2c7259ca41eb3d66e6dd
DESKTOP-6PSTR3V\vanes <vanessa.kirch88@gmail.com>
2019-03-17 15:27:33 -0600

    Variation 1 & 2 added

--- CPSC583 - Project/Variation2/main.js
+++ CPSC583 - Project/Variation2/main.js
@@ -90,7 +90,7 @@ function setupLifeBars()
                 _data_life[i].Country + "</text>";
         }
         else {
-            label = "<text y='" + i * 61 + "' x='" + (_vis_height - PADDING_FOR_LABELS + 200) + "' style='color: white;'>" +
+            var label = "<text y='" + i * 61 + "' x='" + (_vis_height - PADDING_FOR_LABELS + 200) + "' " + "style='color: white;'>" +
                 _data_life[i].Country + "</text>";
         }
 
@@ -165,13 +165,11 @@ function changeBarLengthLife(attr, maxAttrValue){
 function changeBarLengthFertility(attr, maxAttrValue){
     for (var i = 0; i < _data_fertility.length; i++){
         var newLength = mapValue(_data_fertility[i][attr], 0, maxAttrValue, 0, _vis_width/3 - PADDING_FOR_LABELS);
-        newLength = -1*newLength;
-        console.log(newLength);
         var bar = document.getElementById("row_F_" + i);
 
-        var oldX = bar.getAttribute("x"); //- newLength; //FOR NEW FUNCTION
+        var oldX = bar.getAttribute("x");
         var oldLength = bar.getAttribute("width");
-        var newX = oldX;
+        var newX = oldX - newLength;
 
         bar.setAttribute("x", oldX);
         bar.setAttribute("width", oldLength);
@@ -201,3 +199,58 @@ function changeBarLengthFertility(attr, maxAttrValue){
 function mapValue(value, origMin, origMax, newMin, newMax){
     return (value - origMin) * (newMax - newMin) / (origMax - origMin) + newMin;
 }
+
+/*
+
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// set the ranges
+var y = d3.scaleBand()
+    .range([height, 0])
+    .padding(0.1);
+
+var x = d3.scaleLinear()
+    .range([0, width]);
+
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// format the data
+data.forEach(function(d) {
+    d.sales = +d.sales;
+});
+
+// Scale the range of the data in the domains
+x.domain([0, d3.max(data, function(d){ return d.sales; })])
+y.domain(data.map(function(d) { return d.salesperson; }));
+//y.domain([0, d3.max(data, function(d) { return d.sales; })]);
+
+// append the rectangles for the bar chart
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    //.attr("x", function(d) { return x(d.sales); })
+    .attr("width", function(d) {return x(d.sales); } )
+    .attr("y", function(d) { return y(d.salesperson); })
+    .attr("height", y.bandwidth());
+
+// add the x Axis
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// add the y Axis
+svg.append("g")
+    .call(d3.axisLeft(y));
+*/
--- CPSC583 - Project/Variation3/main.js
+++ CPSC583 - Project/Variation3/main.js
@@ -16,7 +16,6 @@ window.onload = function(){
  * Global variables
  */
 var _data_life;
-var _data_fertility;
 var _vis;   // the svg container where we will draw our visualization
 var _vis_width = 0;     // dimensions of the visualization
 var _vis_height = 0;
@@ -41,7 +40,7 @@ function setup(){
  * Function loadData: loads data from a given CSV file path/url
  * @param path string location of the CSV data file
  */
-function loadData(path1, path2){
+function loadData(path1){
 
     if(path1 === "life_expectancy_data_csv.csv"){
         console.log("Dealing with life expectancy data");
@@ -50,13 +49,6 @@ function loadData(path1, path2){
         d3.csv(path1).then(function(data){
             _data_life = data;
             setupLifeBars();    // putting this here to make sure it gets called after the data is loaded
-        });
-    }
-    if(path2 === "fertility_rate_data_csv.csv"){
-        console.log("Dealing with fertility rate data");
-        d3.csv(path2).then(function(data){
-            _data_fertility = data;
-            setUpFertilityBars();
         });
     }
 
@@ -90,7 +82,7 @@ function setupLifeBars()
                 _data_life[i].Country + "</text>";
         }
         else {
-            label = "<text y='" + i * 61 + "' x='" + (_vis_height - PADDING_FOR_LABELS + 200) + "' style='color: white;'>" +
+            var label = "<text y='" + i * 61 + "' x='" + (_vis_height - PADDING_FOR_LABELS + 200) + "' " + "style='color: white;'>" +
                 _data_life[i].Country + "</text>";
         }
 
@@ -100,29 +92,6 @@ function setupLifeBars()
     }
 }
 
-/**
- * Function setupBars: sets up rectangles to represent each data point in our dataset
- */
-
-function setUpFertilityBars()
-{
-    // divide the height of our vis to the number of rows
-    var y_pad = Math.floor(_vis_height / (_data_fertility.length));
-
-    // for each item in our dataset, create a row on the y-axis and label it
-    for (var i = 0; i < (_data_fertility.length); i++){
-        // create a string that follows the HTML tag for drawing a rectangle
-        // e.g., <rect x='0' y='0' width='100' height='100' style='fill:black; strokewidth:0;'></rect>
-        // the rectangle's ID should match an index within our data (e.g., row_0 corresponds to _data[0])
-        // default height is the height of the visualization - some padding for labels
-        var rect = "<rect id='row_F_" + i + "' x='" + ((_vis_width/2)-PADDING_FOR_LABELS) +"' y='" + i * y_pad + "' width='" + (y_pad / 1.5)  + "' height='" +
-            50 + "' style='fill:#FFDC19; stroke-width: 1; stroke:white;'></rect>";
-
-        //append the line within the SVG container element
-        _vis.innerHTML += rect;
-
-    }
-}
 
 /**
  * Function changeBarHeights: changes the heights of the bars in our visualization with values
@@ -152,40 +121,6 @@ function changeBarLengthLife(attr, maxAttrValue){
             "<title>"+ _data_life[i][attr] +"</title>";
         bar.innerHTML = animate;
         document.getElementById('animate_bar_' + i).beginElement();
-    }
-}
-
-/**
- * Function changeBarHeights: changes the heights of the bars in our visualization with values
- * from a given data attribute
- * @param attr the data attribute containing the value to be applied to each bar's height
- * @param maxAttrValue the maximum value the attribute can have
- * assume the min value is 0
- */
-function changeBarLengthFertility(attr, maxAttrValue){
-    for (var i = 0; i < _data_fertility.length; i++){
-        var newLength = mapValue(_data_fertility[i][attr], 0, maxAttrValue, 0, _vis_width/3 - PADDING_FOR_LABELS);
-        newLength = -1*newLength;
-        console.log(newLength);
-        var bar = document.getElementById("row_F_" + i);
-
-        var oldX = bar.getAttribute("x"); //- newLength; //FOR NEW FUNCTION
-        var oldLength = bar.getAttribute("width");
-        var newX = oldX;
-
-        bar.setAttribute("x", oldX);
-        bar.setAttribute("width", oldLength);
-        
-
-        var animate = "<animate id='animate_bar_" + i + "' attributeName='x' from='" + oldX + "' " +
-            "to='" + newX + "' dur='1s' begin='indefinite'" +
-            "repeatCount='1' fill='freeze'></animate>" +
-            "<animate attributeName='width' from='"+ oldLength +"' to='"+ newLength +"' dur='1s' " +
-            "begin='animate_bar_"+ i +".begin' fill='freeze'></animate>" +
-            "<title>"+ _data_fertility[i][attr] +"</title>";
-        bar.innerHTML = animate;
-        document.getElementById('animate_bar_' + i).beginElement();
-
     }
 }
 
